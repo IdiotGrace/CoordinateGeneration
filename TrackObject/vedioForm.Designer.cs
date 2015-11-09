4b5d628bf7d78bb4ae9f239c8c1f689bb614bedc
IdiotGrace <haowenchao123@gmail.com>
2015-11-09 23:02:24 +0100

    initial version since 2015-11-09

---
+++ TrackObject/vedioForm.Designer.cs
@@ -0,0 +1,234 @@
+﻿namespace TrackObject
+{
+    partial class vedioForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.recording = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.coordinateText = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.tbDes = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.cbSpeed = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(23, 24);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 107);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(748, 459);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(23, 66);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(607, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Visible = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(421, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 12);
+            this.label3.TabIndex = 6;
+            // 
+            // recording
+            // 
+            this.recording.AutoSize = true;
+            this.recording.Location = new System.Drawing.Point(364, 74);
+            this.recording.Name = "recording";
+            this.recording.Size = new System.Drawing.Size(78, 16);
+            this.recording.TabIndex = 1;
+            this.recording.Text = "Recording";
+            this.recording.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(488, 71);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 12);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Current Coordinate:";
+            // 
+            // coordinateText
+            // 
+            this.coordinateText.Enabled = false;
+            this.coordinateText.Location = new System.Drawing.Point(613, 67);
+            this.coordinateText.Name = "coordinateText";
+            this.coordinateText.Size = new System.Drawing.Size(121, 21);
+            this.coordinateText.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(518, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Current Time:";
+            this.label1.Visible = false;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(362, 29);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(53, 12);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Seconds:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(123, 74);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(77, 12);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Description:";
+            // 
+            // tbDes
+            // 
+            this.tbDes.Location = new System.Drawing.Point(206, 68);
+            this.tbDes.Name = "tbDes";
+            this.tbDes.Size = new System.Drawing.Size(116, 21);
+            this.tbDes.TabIndex = 12;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(158, 34);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 12);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Speed:";
+            // 
+            // cbSpeed
+            // 
+            this.cbSpeed.FormattingEnabled = true;
+            this.cbSpeed.Items.AddRange(new object[] {
+            "3.0",
+            "2.5",
+            "2.0",
+            "1.5",
+            "1.0",
+            "0.75",
+            "0.5",
+            "0.25",
+            "0.125"});
+            this.cbSpeed.Location = new System.Drawing.Point(206, 26);
+            this.cbSpeed.Name = "cbSpeed";
+            this.cbSpeed.Size = new System.Drawing.Size(121, 20);
+            this.cbSpeed.TabIndex = 14;
+            // 
+            // vedioForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(801, 593);
+            this.Controls.Add(this.cbSpeed);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.tbDes);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.coordinateText);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.recording);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.startButton);
+            this.KeyPreview = true;
+            this.Name = "vedioForm";
+            this.Text = "Press R To Start Recording!";
+            this.Load += new System.EventHandler(this.vedioForm_Load_1);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.vedioForm_KeyDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.vedioForm_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox recording;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox coordinateText;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox tbDes;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox cbSpeed;
+    }
+}
+
---
+++ TrackObject/vedioForm.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.IO;
+using PlayVideoTest;
+
+namespace TrackObject
+{
+    public partial class vedioForm : Form
+    { 
+        public vedioForm()
+        {
+            InitializeComponent();
+            this.KeyPreview = true;
+            this.AutoSize = true;
+            
+            MI = new MediaInfo();
+            fileName = "D:\\Soccer.mp4";//define the file name to play
+            MI.Open(fileName);
+            VideoWidth = Convert.ToInt32(MI.Get(StreamKind.Video, 0, "Width"));
+            VideoHeight = Convert.ToInt32(MI.Get(StreamKind.Video, 0, "Height"));
+            duration = Convert.ToInt32(MI.Get(StreamKind.Video, 0, "Duration"));
+            MI.Close();
+            pictureBox1.Width = (int)((double)VideoWidth*0.8);
+            pictureBox1.Height = (int)((double)VideoHeight*0.8);
+            picX = pictureBox1.Location.X;
+            picY = pictureBox1.Location.Y;
+            picHeight = pictureBox1.Height;
+            picWidth = pictureBox1.Width;
+
+            cbSpeed.SelectedIndex = 4;
+        }
+
+        private void vedioForm_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        //Define Record to store the coordinate and time
+        struct Record
+        {
+            private Point coordinate;
+            public Point Coordinate
+            {
+                get { return coordinate; }
+                set { coordinate = value; }
+            }
+
+            private String time;
+            public String Time
+            {
+                get { return time; }
+                set { time = value; }
+            }
+
+            public Record(Point _coordinate, String _time)
+            {
+                this.coordinate = _coordinate;
+                this.time = _time;
+            }
+
+        }
+
+        List<Record> recordList = new List<Record>();
+
+        DateTime startTime = new DateTime();//time for starting
+        DateTime stopTime = new DateTime();//time for ending
+        System.Timers.Timer Timers_Timer = new System.Timers.Timer();//Definition of Timer   
+        private MediaInfo MI;
+        private String fileName;
+        int picX;
+        int picY;
+        int picHeight;
+        int picWidth;
+        private int duration;
+        private int VideoWidth;
+        private int VideoHeight;
+        void writeInFile(List<Record> recordList)
+        {
+            string fullPath = "D:\\"+tbDes.Text+"-Record.txt";
+            StreamWriter sw;
+            if (!File.Exists(fullPath))
+            {
+                //if file does not exist, then create a new one and wirte the content 
+                sw = File.CreateText(fullPath);
+                sw.Write("<" + tbDes.Text + ">#");
+                for (int i = 0; i < recordList.Count; i++)
+                {
+                    sw.Write("<" + recordList[i].Time + "," + Convert.ToInt64((Convert.ToDecimal(recordList[i].Coordinate.X) / Convert.ToDecimal(picWidth))*10000) + "," + Convert.ToInt64((Convert.ToDecimal(recordList[i].Coordinate.Y)/Convert.ToDecimal(picHeight))*10000) + ">#");
+                }
+            }
+            else
+            {
+                File.Delete(fullPath);
+                //if file already exists, append content 
+                sw = File.CreateText(fullPath);
+                sw.Write("<" + tbDes.Text + ">#");
+                for (int i = 0; i < recordList.Count; i++)
+                {
+                    sw.Write("<" + recordList[i].Time + "," + Convert.ToInt64((Convert.ToDecimal(recordList[i].Coordinate.X) / Convert.ToDecimal(picWidth)) * 10000) + "," + Convert.ToInt64((Convert.ToDecimal(recordList[i].Coordinate.Y) / Convert.ToDecimal(picHeight)) * 10000) + ">#");
+                }
+            }
+            sw.Close();
+        }
+
+        void Timers_Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            label2.Text = Convert.ToString(DateTime.Now - startTime);//update current time to lable  
+            TimeSpan spanStart = DateTime.Now.Subtract(startTime);
+            double milliSecondsStartTime = spanStart.TotalSeconds;
+            TimeSpan spanNow = DateTime.Now.Subtract(DateTime.Now);
+            double milliSecondsNowTime = spanNow.TotalSeconds;
+            label3.Text = Convert.ToString((milliSecondsStartTime - milliSecondsNowTime) * Convert.ToDouble(cbSpeed.Text));
+            if ((milliSecondsStartTime - milliSecondsNowTime) * 1000 * Convert.ToDouble(cbSpeed.Text) > duration)
+            {
+                Timers_Timer.Enabled = false;
+                writeInFile(recordList);//write record to file
+            }
+            if (isRecording)
+            {
+                Point ps = MousePosition;//point according to screen
+                Point pw = this.PointToClient(ps);//point according to windows
+                Point pr = new Point(pw.X - picX, pw.Y - picY);//the real coordinate according to pictureBox1
+                if (pr.X < 0 || pr.X > picWidth || pr.Y < 0 || pr.Y > picHeight)
+                {
+                    coordinateText.Text = "Out of Bound!";
+                }
+                else
+                {
+                    coordinateText.Text = "(" + pr.X + "," + pr.Y + ")";
+                    Record record = new Record(pr, Convert.ToString(Convert.ToInt64((milliSecondsStartTime - milliSecondsNowTime) * 1000 * Convert.ToDouble(cbSpeed.Text))));
+                    recordList.Add(record);
+                }
+            }
+            Graphics g = Graphics.FromImage(pictureBox1.Image);
+            Pen p = new Pen(Color.Red);
+            g.DrawEllipse(p, 0, 0, 100, 100);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            PictureBox PlayScreen = pictureBox1;
+            string mciCommand;
+            mciCommand = "open " + " "+fileName+" " + " alias MyAVI ";
+            mciCommand = mciCommand + " parent " + PlayScreen.Handle.ToInt32() + " style child ";
+            LibWrap.mciSendString(mciCommand, null, 0, 0);
+            Rectangle r = PlayScreen.ClientRectangle;
+            mciCommand = " put MyAVI window at 0 0 " + r.Width + " " + r.Height;
+            LibWrap.mciSendString(mciCommand, null, 0, 0);
+            LibWrap.mciSendString(" set MyAVI speed "+1000*Convert.ToDouble(cbSpeed.Text), null, 0, 0);
+            LibWrap.mciSendString(" play MyAVI ", null, 0, 0);
+            //Set Timer and start playing video   
+            Timers_Timer.Interval = (int)(100.0/Convert.ToDouble(cbSpeed.Text));
+            Timers_Timer.Enabled = true;
+            Timers_Timer.Elapsed += new System.Timers.ElapsedEventHandler(Timers_Timer_Elapsed);
+            startTime = DateTime.Now;//这边得稍微改一下。因为不小心又按到了start键的话，计时就重新开始了
+        }
+
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            //stop playing video   
+            Timers_Timer.Enabled = false;
+            stopTime = DateTime.Now;
+            //write record to file
+            writeInFile(recordList);
+            LibWrap.mciSendString(" stop MyAVI ", null, 0, 0);
+        }
+
+        Boolean isRecording = false;//initialize the flag of isRecording
+        //press space key and start or end record coordinate
+        private void vedioForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.R)
+            {
+                
+                isRecording = !isRecording;
+                recording.Checked = isRecording;
+            }
+        }
+
+        //track mouse
+        private Point delta;
+        private Boolean deltaInitial = false;
+        private void vedioForm_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            if (deltaInitial)
+                return;
+
+            Point ps = MousePosition;//point according to screen
+            Point pw = this.PointToClient(ps);//point according to windows
+            delta.X = pw.X - e.X;
+            delta.Y = pw.Y - e.Y;
+            deltaInitial = true;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void vedioForm_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+    public class LibWrap
+    {
+        [DllImport(("winmm.dll "), EntryPoint = "mciSendString", CharSet = CharSet.Auto)]
+        public static extern int mciSendString(string lpszCommand, string lpszReturnString,
+                    uint cchReturn, int hwndCallback);
+    }
+
+
+}
